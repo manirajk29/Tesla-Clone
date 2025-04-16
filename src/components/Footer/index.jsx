d9fb191fa5ab56056583d747f1e6a1407754ca4e
manirajk29 <manirajkvm29@gmail.com>
2025-04-16 11:56:51 +0530

    2nd

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,11 +8,11 @@ const Footer = ({bottom}) =>{
         <Wrapper>
             <Content className={bottom} >
                 <div>
-                    <a href="https://github.com/chirag-23/Tesla-clone-reactjs" target='_blank' >Tesla-Clone &copy; 2025</a>
+                    <a href="https://github.com/Manirajk29/Tesla-clone" target='_blank' >Tesla-Clone &copy; 2025</a>
                 </div>
                 <div>
-                    <a href="https://www.linkedin.com/in/chirag-chouhan-b48311228/" target="_blank" >LinkedIn</a>
-                    <a href="https://github.com/chirag-23" target="_blank" >GitHub</a>
+                    <a href="https://www.linkedin.com/in/Maniraj/" target="_blank" >LinkedIn</a>
+                    <a href="https://github.com/Manirajk29" target="_blank" >GitHub</a>
                 </div>
             </Content>
         </Wrapper>
